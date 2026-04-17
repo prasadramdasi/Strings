47c5676d0fb24f948af8e2e16ef2ca5a7fb2363e
@prasad <99307972+prasadramdasi@users.noreply.github.com>
2026-04-17 21:32:43 +0530

    Write a program to print the reverse of the String?
     Write a program to print the reverse of the String?
    Ex: Input - Nacre  
         Output - ercaN

---
+++ Reverse.java
@@ -0,0 +1,16 @@
+public class Reverse { 
+ 
+ public static void main(String[] args) { 
+   
+  Scanner sc = new Scanner(System.in); 
+  System.out.println("Enter String to reverse: "); 
+   
+  String str = sc.nextLine(); 
+  for(int i=str.length()-1;i>=0;i--) { 
+   System.out.print(str.charAt(i)); 
+  } 
+  sc.close(); 
+   
+ } 
+ 
+}
